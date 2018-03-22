f2669f20b3285f8937e234ff5967b7476cdd87ca
Mukesh Patel <37759@hexaware.com>
2018-03-22 13:23:43 +0530

    routing changes don

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ComposeMessageComponent } from './compose-message.component';
+import { CanDeactivateGuard } from './can-deactivate-guard.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth-guard.service';
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
+
+const appRoutes: Routes = [
+    {
+        path: 'compose',
+        component: ComposeMessageComponent,
+        outlet: 'popup'
+    },
+    {
+        path: 'admin',
+        loadChildren: 'app/admin/admin.module#AdminModule',
+        canLoad: [AuthGuard]
+    },
+    {
+        path: 'crisis-center',
+        loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule',
+        data: { preload: false }
+    },
+    { path: '', redirectTo: '/superheroes', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            {
+                enableTracing: true,
+                preloadingStrategy: SelectivePreloadingStrategy
+            } // <-- debugging purposes only
+        )
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: [
+        CanDeactivateGuard,
+        SelectivePreloadingStrategy
+    ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroesModule } from './heroes/heroes.module';
+// import { CrisisCenterModule } from './crisis-center/crisis-center.module';
+import { ComposeMessageComponent } from './compose-message.component';
+// import { AdminModule } from './admin/admin.module';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+
+import { DialogService } from './dailog.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    ComposeMessageComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HeroesModule,
+    // CrisisCenterModule,
+    BrowserAnimationsModule,
+    // AdminModule,
+    LoginRoutingModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DialogService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
+
